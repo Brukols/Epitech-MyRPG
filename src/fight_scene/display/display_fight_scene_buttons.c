c454e8eb1d97ba4f50ba85bc2a52cae8a10b69f1
Paul Van Kerckvoorde <PaulVK@localhost.localdomain>
2019-05-02 11:23:50 +0200

    Makefile changed

--- src/fight_scene/display/display_fight_scene_buttons.c
+++ src/fight_scene/display/display_fight_scene_buttons.c
@@ -11,9 +11,8 @@ void display_fight_scene_buttons(game_t *game)
 {
     buttons_t *buttons = game->scenes->buttons;
 
-    for (; buttons->prev; buttons = buttons->prev);
+//    for (; buttons->prev; buttons = buttons->prev);
     for (; buttons; buttons = buttons->next) {
-        sfSprite_setTextureRect(buttons->sprite, buttons->rect);
         sfRenderWindow_drawSprite(game->window, buttons->sprite, NULL);
     }
 }
