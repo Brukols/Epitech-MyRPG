4f9629f9f5d85c2ea81d52ce82727e5772b0ecb6
Paul Van Kerckvoorde <PaulVK@localhost.localdomain>
2019-05-04 16:21:02 +0200

    cone create

--- src/general/initialization/init_cone.c
+++ src/general/initialization/init_cone.c
@@ -16,9 +16,9 @@ game_object_t *init_cone(game_object_t *go, int x, int y)
         return (NULL);
     go->pos = init_vec2f(x * 2.3, y * 2.3);
     go->comparison = y * 2.3 + 50;
-    go->rect = init_intrect(1040, 0, 50, 94);
-    go->hitbox_pos = init_vec2f(x * 2.3, (y + 60) * 2.3);
-    go->hitbox_size = init_vec2f(40 * 2.3, 40 * 2.3);
+    go->rect = init_intrect(1030, 0, 50, 94);
+    go->hitbox_pos = init_vec2f((x + 10) * 2.3, (y + 60) * 2.3);
+    go->hitbox_size = init_vec2f(35 * 2.3, 40 * 2.3);
     go->scale = init_vec2f(2.3, 2.3);
     go->type = CONE;
     go->display = true;
