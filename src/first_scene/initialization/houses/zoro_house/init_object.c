b2abd7cc9f7896d7c07f7720ec60aedbe78d1fca
Mai Ly Lehoux <Pasteque@localhost.localdomain>
2019-05-05 15:50:50 +0200

    rm *.o

--- src/first_scene/initialization/houses/zoro_house/init_object.c
+++ src/first_scene/initialization/houses/zoro_house/init_object.c
@@ -15,11 +15,11 @@ game_object_t *init_table(game_object_t *go, int x, int y)
     if (!(go->sprite = sfSprite_create()))
         return (NULL);
     go->pos = init_vec2f(x * 4.3, y * 4.3);
-    go->comparison = y * 4.3;
+    go->comparison = (y - 20) * 4.3;
     go->display = true;
     go->rect = init_intrect(806, 196, 54, 55);
-    go->hitbox_pos = init_vec2f(x * 4.3, y * 4.3);
-    go->hitbox_size = init_vec2f(24 * 4.3, 31 * 4.3);
+    go->hitbox_pos = init_vec2f((x + 14) * 4.3 ,(y + 14) * 4.3);
+    go->hitbox_size = init_vec2f(25 * 4.3, 26 * 4.3);
     go->scale = init_vec2f(4.3, 4.3);
     go->type = TABLE;
     go->interaction = false;
